5af319bd73112720d5c29f53a9b837806b781e96
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-10-10 16:01:53 +0700

    add testcase 31/01/20108

--- src/main/java/com/example/democi/CaculatorNextDay.java
+++ src/main/java/com/example/democi/CaculatorNextDay.java
@@ -2,7 +2,13 @@ package com.example.democi;
 
 public class CaculatorNextDay {
     public static String nextDayCalculator(int inputDay, int inputMonth, int inputYear){
-        inputDay++;
+        if (inputDay == 31){
+            inputDay = 1;
+            inputMonth = inputMonth ++;
+        }
+        else {
+            inputDay++;
+        }
         return inputDay + "-" + inputMonth + "-" + inputYear;
     }
 }
