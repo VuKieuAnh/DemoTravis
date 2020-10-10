7625304f2441458d85f0d258385aebe3bc16aeb8
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-10-10 14:37:11 +0700

    add testcase 31/01/20108

--- src/test/java/com/example/democi/CaculatorNextDayTest.java
+++ src/test/java/com/example/democi/CaculatorNextDayTest.java
@@ -17,4 +17,17 @@ class CaculatorNextDayTest {
         assertEquals(expected, result);
     }
 
+    @Test
+    @DisplayName("case 31/1/2018")
+    void testDay31Month1Year2018(){
+        int dayTest = 1;
+        int monthTest = 1;
+        int yearTest = 2018;
+        String result = CaculatorNextDay.nextDayCalculator(dayTest, monthTest, yearTest);
+        String expected = "1-2-2018";
+        assertEquals(expected, result);
+    }
+
+
+
 }
