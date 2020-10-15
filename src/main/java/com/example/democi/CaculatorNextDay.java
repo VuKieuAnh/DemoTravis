d38a0223ea37c988739aa2d379cf64a4229f8732
VuKieuAnh <vukieuanh.hnue@gmail.com>
2020-10-15 10:42:20 +0700

    add case 29/2/2020

--- src/main/java/com/example/democi/CaculatorNextDay.java
+++ src/main/java/com/example/democi/CaculatorNextDay.java
@@ -1,14 +1,50 @@
 package com.example.democi;
 
 public class CaculatorNextDay {
+
+    public static final int FISTOFMONTH = 1;
+
     public static String nextDayCalculator(int inputDay, int inputMonth, int inputYear){
-        if (inputDay == 31){
-            inputDay = 1;
+        if (inputDay == getLastDayOfMonth(inputMonth, inputYear)){
+            inputDay = FISTOFMONTH;
             inputMonth = ++inputMonth;
         }
         else {
             inputDay++;
         }
         return inputDay + "-" + inputMonth + "-" + inputYear;
+    }
+
+    //input: inputMonth (int) + inputYear (int)
+    //output: lastDayOfMonth (int)
+    //name: getLastDayOfMonth
+
+    private static int getLastDayOfMonth(int inputMonth, int inputYear){
+        switch (inputMonth){
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                return 31;
+            case 2:
+                return isLeapYear(inputYear)? 29:28;
+        }
+        return 30;
+    }
+
+    private static boolean isLeapYear (int inputYear){
+        if(inputYear % 4 == 0){
+            if (inputYear % 100 == 0){
+                if (inputYear % 400 == 0){
+                    return true;
+                }
+                else return false;
+            }
+            else return true;
+        }
+        else return false;
     }
 }
--- src/test/java/com/example/democi/CaculatorNextDayTest.java
+++ src/test/java/com/example/democi/CaculatorNextDayTest.java
@@ -9,11 +9,17 @@ class CaculatorNextDayTest {
     @Test
     @DisplayName("case 1/1/2018")
     void testDay1Month1Year2018(){
+
+        //AAA
+        //arange
         int dayTest = 1;
         int monthTest = 1;
         int yearTest = 2018;
-        String result = CaculatorNextDay.nextDayCalculator(dayTest, monthTest, yearTest);
         String expected = "2-1-2018";
+
+        //action
+        String result = CaculatorNextDay.nextDayCalculator(dayTest, monthTest, yearTest);
+        //assert
         assertEquals(expected, result);
     }
 
@@ -28,6 +34,35 @@ class CaculatorNextDayTest {
         assertEquals(expected, result);
     }
 
-
+    @Test
+    @DisplayName("case 30/04/2018")
+    void testDay30Month4Year2018(){
+        int dayTest = 30;
+        int monthTest = 4;
+        int yearTest = 2018;
+        String expected = "1-5-2018";
+        String result = CaculatorNextDay.nextDayCalculator(dayTest, monthTest, yearTest);
+        assertEquals(expected, result);
+    }
+    @Test
+    @DisplayName("case 28/02/2018")
+    void testDay28Month2Year2018(){
+        int dayTest = 28;
+        int monthTest = 2;
+        int yearTest = 2018;
+        String expected = "1-3-2018";
+        String result = CaculatorNextDay.nextDayCalculator(dayTest, monthTest, yearTest);
+        assertEquals(expected, result);
+    }
+    @Test
+    @DisplayName("case 29/02/2020")
+    void testDay29Month2Year2020(){
+        int dayTest = 29;
+        int monthTest = 2;
+        int yearTest = 2020;
+        String expected = "1-3-2020";
+        String result = CaculatorNextDay.nextDayCalculator(dayTest, monthTest, yearTest);
+        assertEquals(expected, result);
+    }
 
 }
